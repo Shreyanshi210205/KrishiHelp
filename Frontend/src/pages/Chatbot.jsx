480d37da8af254a861ce487093d20178eacd77ef
Shreyanshi210205 <shreyanshi.230101129@iiitbh.ac.in>
2025-06-10 00:25:18 +0530

    "added chatbot"

--- Frontend/src/pages/Chatbot.jsx
+++ Frontend/src/pages/Chatbot.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import React from 'react';
+import './Chatbot.css';
 
 const Chatbot = () => {
-  return <h2>Chatbot page</h2>;
+  return (
+    <div className="chatbot-overlay">
+      <div className="chatbot-message">
+        <h1>👋 Hi, I'm <span className="highlight">Bhoomi AI</span></h1>
+        <p>
+          I'm your intelligent farming assistant here to guide you with tips, tools, and support for a better, greener future. 🌿
+        </p>
+        <button className="chatbot-btn">💬 Let's Chat</button>
+      </div>
+    </div>
+  );
 };
 
 export default Chatbot;
